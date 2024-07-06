f22f2e334d11ddabc808fa465997385d394700e0
Mykhailo Marchuk <mishaakamichael999@gmail.com>
2024-07-06 18:27:24 +0300

    Initial commit, create project

---
+++ src/test/java/marchuk/csv/processor/CsvProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package marchuk.csv.processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsvProcessorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
